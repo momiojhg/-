00a5fd62460a87a215ee89cd7034583efb7cb901
Momio <117796198+momiojhg@users.noreply.github.com>
2025-02-18 00:00:15 +0800

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+//	stack<int> st;
+	//top()¡¢push() 
+//	for (int i = 1; i <= 5; i ++){
+//		st.push(i);
+//	}
+//	printf("%d\n", st.top());
+//	//pop() 
+//	for (int i = 1; i <= 3; i ++){
+//		st.pop();
+//	} 
+//	printf("%d\n", st.top());
+	//empty()
+//	stack<int> st1;
+//	if(st1.empty() == true){
+//		printf("Empty\n");
+//	}else{
+//		printf("Not Empty\n");
+//	}
+//	st.push(1);
+//	if(st.empty() == true){
+//		printf("Empty\n");
+//	}else{
+//		printf("Not Empty\n");
+//	}
+	//size()
+//	stack<int> st;
+//	for(int i = 1; i <= 5; i ++){
+//		st.push(i);
+//	}
+//	printf("%d\n", st.size());
+
+	
+	return 0;
+} 
